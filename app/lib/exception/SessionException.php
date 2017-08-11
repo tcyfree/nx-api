816021ee2466d93ab16187bbd1f8f8c17a0b49cb
tcyfree <1946644259@qq.com>
2017-08-11 16:32:08 +0800

    添加session校验

---
+++ app/lib/exception/SessionException.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkNuan-x [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017-2018 http://www.nuan-x.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: tcyfree <1946644259@qq.com>
+// +----------------------------------------------------------------------
+
+namespace app\lib\exception;
+
+
+class SessionException extends BaseException
+{
+    public $code = 404;
+    public $msg = 'session过期了';
+    public $errorCode = 10011;
+}
