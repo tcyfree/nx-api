d8008757c9c732fd7e53eeaeba19b959f634ee58
tcyfree <tcy13355076>
2017-08-15 10:50:49 +0800

    获取用户信息

--- app/api/model/UserInfo.php
+++ app/api/model/UserInfo.php
@@ -12,6 +12,12 @@ namespace app\api\model;
 class UserInfo extends BaseModel
 {
     protected $autoWriteTimestamp = true;
+    protected $hidden = ['create_time','update_time','openid','unionid',];
+    //读取器自动拼接图片URL
+    public function getAvatarAttr($value){
+//        return $this->prefixImgUrl($value, $data);
+        return config('setting.img_prefix').$value;
+    }
     public static function getByOpenID($unionid)
     {
         $user = self::where('unionid', '=', $unionid)
