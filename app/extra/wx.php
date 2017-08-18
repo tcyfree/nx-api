683ab2de214702b00bd07f4ec9fbfb56c9aa2f07
BeyondX16 <1946644259@qq.com>
2017-08-18 11:22:08 +0800

    Update wx.php

--- app/extra/wx.php
+++ app/extra/wx.php
@@ -8,8 +8,8 @@
 
 return [
     //PC扫码登录
-    'app_id'     => 'wx21128d939f7e9487',
-    'app_secret' => 'bdd010cd89ed5c743e45e234b51691ea',
+    'app_id'     => '',
+    'app_secret' => '',
     'qr_code'    => 'https://open.weixin.qq.com/connect/qrconnect?'.
         'appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_login&state=%s#wechat_redirect',
     'qr_access_token' => "https://api.weixin.qq.com/sns/oauth2/access_token?".
@@ -17,8 +17,8 @@ return [
     'qr_userinfo'      => 'https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s',
 
     //微信App授权登录
-    'g_app_id' => 'wxcfe1a677025cf432',
-    'g_app_secret' => '5f1ce56ce174f5281f9032f4bceeb4fd',
+    'g_app_id' => '',
+    'g_app_secret' => '',
     'g_code'  => 'https://open.weixin.qq.com/connect/oauth2/authorize?'.
         'appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_userinfo&state=%s#wechat_redirect ',
     'g_access_token' => 'https://api.weixin.qq.com/sns/oauth2/access_token?'.
