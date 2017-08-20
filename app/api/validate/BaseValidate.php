29ae0c2f19e02783cddf3f531d1fd02939be023a
tcyfree <1946644259@qq.com>
2017-08-20 19:44:03 +0800

    更新手机号验证模式

--- app/api/validate/BaseValidate.php
+++ app/api/validate/BaseValidate.php
@@ -63,7 +63,7 @@ class BaseValidate extends Validate
 
     protected function isMobile($value)
     {
-        $rule = '^1(3|4|5|7|8)[0-9]\d{8}$^';
+        $rule = '/^1(3|4|5|7|8)\d{9}$/';
         $result = preg_match($rule, $value);
         if ($result) {
             return true;
