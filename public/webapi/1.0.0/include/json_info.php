22d9594ae81dac5c81401c0385da04453cb235a7
tcyfree <tcy13355076>
2017-08-22 11:39:50 +0800

    封装API文档分页通用脚本

---
+++ public/webapi/1.0.0/include/json_info.php
@@ -0,0 +1,12 @@
+<p class="subtitlestyle">（三）服务接口响应请求：</p>
+<table width="90%" border="1" class="dbTable">
+    <tr class="td_header">
+        <td width="51%">响应结果</td>
+        <td width="31%">备注</td>
+    </tr>
+    <tr>
+        <td><p>{json信息串}</p></td>
+        <td><p>详见（四）特别备注</p></td>
+    </tr>
+    <?php require_once("error.inc.php");?>
+</table>
--- public/webapi/1.0.0/include/page.inc.php
+++ public/webapi/1.0.0/include/page.inc.php
@@ -2,10 +2,10 @@
    <td>page</td>
    <td>当前列表翻页索引</td>
    <!--<td width="53%"><font color="#FF0000">第一页时请传递page=0，翻页时依次递增。</font></td>-->
-   <td width="53%">第一页时请传递page=0，翻页时依次递增。</td>
+   <td width="53%">第一页时请传递page=1，翻页时依次递增。</td>
  </tr>
  <tr class="waitset">
-     <td>page_size</td>
+     <td>size</td>
      <td>定义列表页面数量大小</td>
      <!--<td width="53%"><font color="#FF0000">第一页时请传递page=0，翻页时依次递增。</font></td>-->
      <td width="53%">比如10代表每页请求10条数据</td>
