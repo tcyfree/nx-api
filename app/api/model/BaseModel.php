02dc1a3afb549e60fe1b3d3a88b1dded6d031b73
tcyfree <1946644259@qq.com>
2017-08-30 23:24:34 +0800

    PSR-4,PSR-0 自动加载规范

--- app/api/model/BaseModel.php
+++ app/api/model/BaseModel.php
@@ -6,7 +6,8 @@ use think\Model;
 
 class BaseModel extends Model
 {
-    //PSR-4,PSR-0
+    //PSR-4,PSR-0 自动加载规范，所以不需要显示的去require 某一个.php文件，只有use它的命名空间就可以了。
+
     protected function prefixImgUrl ($value,$data){
         $finalUrl = $value;
         if($data['from'] == 0){
