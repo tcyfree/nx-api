d0f4ac6f7f48ac5b55c9f9506fae774e660b6e3e
tcyfree <tcy13355076>
2017-09-01 09:20:32 +0800

    订单处理

--- app/api/validate/OrderPlace.php
+++ app/api/validate/OrderPlace.php
@@ -9,83 +9,31 @@
 namespace app\api\validate;
 
 
-use app\lib\exception\ParameterException;
+use app\lib\RexExp;
 
 class OrderPlace extends BaseValidate
 {
-    protected $oProducts = [
-        [
-            'product_id' => 1,
-            'count' => 3
-        ],
-        [
-            'product_id' => 2,
-            'count' => 3
-        ],
-        [
-            'product_id' => 3,
-            'count' => 3
-        ]
-    ];
-
-    protected $products = [
-        [
-            'product_id' => 1,
-            'count' => 2
-        ],
-        [
-            'product_id' => 2,
-            'count' => 3
-        ],
-        [
-            'product_id' => 3,
-            'count' => 3
-        ]
-    ];
 
     protected $rule = [
-        'products' => 'checkProducts'
+        'amount' => 'require|amountRexExp'
     ];
 
     protected $singleRule = [
-        'product_id' => 'require|isPositiveInteger',
-        'count' => 'require|isPositiveInteger',
+        'amount.amountRexExp' => '充值金额只能是正整数或小数1位和2位，不能为0或0.0和0.00'
     ];
 
-    protected function checkProducts($values)
+    /**
+     * 充值金额仅匹配正整数或小数1位和2位，不能为0或0.0和0.00
+     * @param $value
+     * @return bool
+     */
+    protected function amountRexExp($value)
     {
-        if (!is_array($values))
-        {
-            throw new ParameterException(
-                [
-                    'msg' => '商品参数不正确'
-                ]);
-        }
-
-        if (empty($values))
-        {
-            throw new ParameterException(
-                [
-                    'msg' => '商品列表不能为空'
-                ]);
-        }
-
-        foreach ($values as $value)
-        {
-            $this->checkProduct($value);
-        }
-        return true;
-    }
-
-    protected function checkProduct($value)
-    {
-        $validate = new BaseValidate($this->singleRule);
-        $result = $validate->check($value);
-        if(!$result){
-            throw new ParameterException([
-                'msg' => '商品列表参数错误',
-            ]);
+        $res = RexExp::justIntegerOrDecimalNotZero($value);
+        if($res == 1) {
+            return true;
+        }else {
+            return false;
         }
     }
-
 }
