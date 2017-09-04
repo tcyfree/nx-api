3abe9685b730341e908cbef69caf128f7fd71eda
tcyfree <tcy13355076>
2017-09-04 13:50:05 +0800

    Token ==> token

--- app/lib/exception/TokenException.php
+++ app/lib/exception/TokenException.php
@@ -12,6 +12,6 @@ namespace app\lib\exception;
 class TokenException extends BaseException
 {
     public $code = 401;
-    public $msg = 'Token已过期或无效Token';
+    public $msg = 'token已过期或无效token';
     public $errorCode = 10001;
 }
