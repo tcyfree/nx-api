58474c092719f94bf6804284ecf22f12164ca44c
tcyfree <tcy13355076>
2017-09-07 18:53:12 +0800

    更新错误编码表

--- public/webapi/1.0.0/phpfiles/error_code.php
+++ public/webapi/1.0.0/phpfiles/error_code.php
@@ -11,125 +11,17 @@
     <td width="63%">描述说明</td>
   </tr>
   <tr>
-    <td>100</td>
-    <td><?php echo $msg["100"]?></td>
+    <td>10000</td>
+    <td><?php echo $msg["10000"]?></td>
   </tr>
-  <tr>
-    <td>101</td>
-    <td><?php echo $msg["101"]?></td>
+
+    <td>10007</td>
+    <td><?php echo $msg["10007"]?></td>
   </tr>
-  <tr>
-    <td>102</td>
-    <td><?php echo $msg["102"]?></td>
-  </tr>
-   <tr>
-    <td>103</td>
-    <td><?php echo $msg["103"]?></td>
-  </tr>
-   <tr>
-    <td>104</td>
-    <td><?php echo $msg["104"]?></td>
-  </tr>
-  <tr>
-    <td>105</td>
-    <td><?php echo $msg["105"]?></td>
-  </tr>
-  <tr>
-    <td>106</td>
-    <td><?php echo $msg["106"]?></td>
-  </tr>
-  <tr>
-    <td>107</td>
-    <td><?php echo $msg["107"]?></td>
-  </tr>
-  <tr>
-    <td>108</td>
-    <td><?php echo $msg["108"]?></td>
-  </tr>
-   <tr>
-    <td>109</td>
-    <td><?php echo $msg["109"]?></td>
-  </tr>
-    <tr class="redStyle">
-        <td>110</td>
-        <td><?php echo $msg["110"]?></td>
-    </tr>
-    <tr>
-        <td>111</td>
-        <td><?php echo $msg["111"]?></td>
-    </tr>
-    <tr>
-        <td>112</td>
-        <td><?php echo $msg["112"]?></td>
-    </tr>
-   <tr class="redStyle">
-       <td>200</td>
-       <td><?php echo $msg["200"]?></td>
-   </tr>
-    <!--
-     <tr>
-    <td>203</td>
-    <td><?php echo $msg["203"]?></td>
-  </tr>
- <tr>
-    <td>204</td>
-    <td><?php echo $msg["204"]?></td>
-  </tr>
-  <tr>
-    <td>205</td>
-    <td><?php echo $msg["205"]?></td>
-  </tr>
-   <tr>
-    <td>207</td>
-    <td><?php echo $msg["207"]?></td>
-  </tr>
-   <tr>
-    <td>208</td>
-    <td><?php echo $msg["208"]?></td>
-  </tr>
-    <tr>
-    <td>209</td>
-    <td><?php echo $msg["209"]?></td>
-  </tr>
-    <tr>
-    <td>206</td>
-    <td><?php echo $msg["206"]?></td>
-  </tr>-->
-  <tr>
-    <td>300</td>
-    <td><?php echo $msg["300"]?></td>
-  </tr>
-  <tr>
-    <td>301</td>
-    <td><?php echo $msg["301"]?></td>
-  </tr>
-  <tr>
-    <td>302</td>
-    <td><?php echo $msg["302"]?></td>
-  </tr>
-<tr>
-    <td>304</td>
-    <td><?php echo $msg["304"]?></td>
-  </tr>
-   <tr>
-       <td>402</td>
-       <td><?php echo $msg["402"]?></td>
-   </tr>
-  <tr>
-    <td>403</td>
-    <td><?php echo $msg["403"]?></td>
-  </tr>
-  <tr>
-    <td>404</td>
-    <td><?php echo $msg["404"]?></td>
-  </tr>
-  <tr>
-    <td>500</td>
-    <td><?php echo $msg["500"]?></td>
-  </tr>
+
   
 </table>
-<p class="inforstyle">提示： 在整个项目当中，所有的error_code，标识和描述说明具有唯一性。 例如： 102 错误编码在任何服务响应当中，都代表“ <?php echo $msg["102"]?>”。</p>
+<p class="inforstyle">提示： 在整个项目当中，所有的error_code，标识和描述说明具有唯一性。 例如： 10000 错误编码在任何服务响应当中，都代表“ <?php echo $msg["10000"]?>”。</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 </div>
