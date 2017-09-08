f93d8bb9419b7b7b5941bdf4061e05be51ac83f9
tcyfree <tcy13355076>
2017-09-08 16:41:34 +0800

    分页

---
+++ public/webapi/1.0.0/include/page.required.php
@@ -0,0 +1,12 @@
+ <tr class="waitset">
+   <td>page</td>
+   <td>当前列表翻页索引</td>
+   <td >是</td>
+   <td >第一页时请传递page=1，翻页时依次递增。</td>
+ </tr>
+ <tr class="waitset">
+     <td>size</td>
+     <td>定义列表页面数量大小</td>
+     <td >是</td>
+     <td >比如10代表每页请求10条数据</td>
+ </tr>
