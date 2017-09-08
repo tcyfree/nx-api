5d935191c21980f8b46766e2a0dc34c78e0f5fed
tcyfree <tcy13355076>
2017-09-08 08:57:32 +0800

    跨域

--- app/api/behavior/CORS.php
+++ app/api/behavior/CORS.php
@@ -15,7 +15,7 @@ class CORS
     {
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
-        header('Access-Control-Allow-Methods: POST,GET');
+        header('Access-Control-Allow-Methods: POST,GET,DELETE,PUT');
         if(request()->isOptions()){
             exit();
         }
