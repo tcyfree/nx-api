f104ddbabd3a22780b801404a7949ed0cd1d67a8
tcyfree <1946644259@qq.com>
2017-09-14 17:06:13 +0800

    定时器：每月将行动社修改次数设为初始值3

---
+++ crontab.php
@@ -0,0 +1,42 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkNuan-x [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017-2018 http://www.nuan-x.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: probe <1946644259@qq.com>
+// +----------------------------------------------------------------------
+// | DateTime: 2017/9/14/16:20
+// +----------------------------------------------------------------------
+
+/**
+ * @param string $url post请求地址
+ * @param array $params
+ * @return mixed
+ */
+function curl_post($url, array $params = array())
+{
+    $data_string = json_encode($params);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt(
+        $ch, CURLOPT_HTTPHEADER,
+        array(
+            'Content-Type: app/json'
+        )
+    );
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return ($data);
+}
+
+$uri = 'http://api.xingdongshe.com/v1/community/init';
+curl_post($uri);
