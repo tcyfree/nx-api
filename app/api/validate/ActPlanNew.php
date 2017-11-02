8f2a29fa2361afd124ae7064b29598e5ab6abf8e
tcyfree <1946644259@qq.com>
2017-11-02 10:08:47 +0800

    必须是整数

--- app/api/validate/ActPlanNew.php
+++ app/api/validate/ActPlanNew.php
@@ -21,7 +21,7 @@ class ActPlanNew extends BaseValidate
         'name'         => 'require|max:50',
         'description'  => 'require|max:140',
         'cover_image'  => 'require|url',
-        'fee'          => 'require|between:1,10000',
+        'fee'          => 'require|between:1,10000|isPositiveInteger',
         'mode'         => 'require|in:0,1'
     ];
 }
