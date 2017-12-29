263f975a60b4285a5d2d72f7eadc3ad59c7a283e
tcyfree <1946644259@qq.com>
2017-12-29 10:03:19 +0800

    4.暂停成员资格不能发条目

--- app/api/controller/v1/Communication.php
+++ app/api/controller/v1/Communication.php
@@ -52,7 +52,7 @@ class Communication extends BaseController
         $uid = TokenService::getCurrentUid();
         CommunityModel::checkCommunityExists($dataArray['community_id']);
         $result = CommunityUserModel::checkCommunityBelongsToUser($uid,$dataArray['community_id']);
-        if ($result['type'] == 2 && $result['pay'] ==0 || $result['status'] ==2){
+        if ($result['type'] == 2 && $result['pay'] == 0 || $result['status'] ==2){
             throw new ParameterException([
                 'msg' => '你不是该行动社付费用户或被暂停成员资格不能发条目哦'
             ]);
