65806d3326eb7b7df38c5a666f540f809a14d606
tcyfree <1946644259@qq.com>
2018-02-01 09:41:18 +0800

    postActPlan: refactoring

--- app/api/controller/v2/ActPlan.php
+++ app/api/controller/v2/ActPlan.php
@@ -21,13 +21,17 @@ use app\api\model\Community as CommunityModel;
 use app\api\model\ActPlan as ActPlanModel;
 use app\api\model\ActPlanRecord as ActPlanRecordModel;
 use app\lib\exception\SuccessMessage;
+use think\Db;
+use think\Exception;
 
 class ActPlan extends BaseController
 {
     /**
      * 创建行动计划
      * 1.鉴权
+     *
      * @return \think\response\Json
+     * @throws Exception
      */
     public function createActPlan()
     {
@@ -37,18 +41,21 @@ class ActPlan extends BaseController
         $dataArray = input('post.');
         $dataArray['id'] = $id;
 
-        $c_obj = new CommunityService();
         $auth_array[0] = 1;
-        $c_obj->checkManagerAuthority($uid,$dataArray['community_id'],$auth_array);
-
+        (new CommunityService())->checkManagerAuthority($uid,$dataArray['community_id'],$auth_array);
         CommunityModel::checkCommunityExists($dataArray['community_id']);
-        ActPlanModel::create($dataArray);
-
-        $data['act_plan_id'] = $id;
-        $data['user_id'] = $uid;
-        $data['type']    = 0;
-        ActPlanRecordModel::create($data);
-
+        Db::startTrans();
+        try{
+            ActPlanModel::create($dataArray);
+            $data['act_plan_id'] = $id;
+            $data['user_id'] = $uid;
+            $data['type']    = 0;
+            ActPlanRecordModel::create($data);
+            Db::commit();
+        }catch (Exception $e){
+            Db::rollback();
+            throw $e;
+        }
         return json(new SuccessMessage(), 201);
     }
 
